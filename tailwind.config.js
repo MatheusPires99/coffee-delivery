dc958e71a128882f81a76c61c0f35a412b420ef5
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-07 13:39:30 -0300

    chore: Config global styles and add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "yellow-light": "#F1E9C9",
+      yellow: "#DBAC2C",
+      "yellow-dark": "#C47F17",
+
+      "purple-light": "#EBE5F9",
+      purple: "#8047F8",
+      "purple-dark": "#4B2995",
+
+      "accent-background": "#FAFAFA",
+      "accent-card": "#F3F2F2",
+      "accent-input": "#EDEDED",
+      "accent-button": "#E6E5E5",
+      "accent-button-hover": "#D7D5D5",
+      "accent-label": "#8D8686",
+      "accent-text": "#574F4D",
+      "accent-subtitle": "#403937",
+      "accent-title": "#272221",
+    },
+    fontFamily: {
+      heading: "'Baloo 2', cursive",
+      body: "'Roboto, sans-serif'",
+    },
   },
   plugins: [],
 };
